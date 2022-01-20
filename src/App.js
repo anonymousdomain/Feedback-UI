033a0c0ef128657fe2d7d5af053edf67e8d83858
anonymousdomain <domainanonymous1@gmail.com>
2022-01-20 10:23:20 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
   const addFfeedback = (newFeedback) => {
-    newFeedback.id=uuidv4()
-    setFeedback([newFeedback,...feedback])
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
   };
   const handleDelete = (id) => {
     setFeedback(feedback.filter((item) => item.id !== id));
   };
   return (
-    <>
+    <Router>
       <Header />
+
       <div className="container">
-        <FeedbackForm handleAdd={addFfeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} onDelete={handleDelete} />
+        <Routes>
+          <Route 
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFfeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} onDelete={handleDelete}  />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink/>
       </div>
-    </>
+    </Router>
   );
 };
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Card from '../components/Shared/Card';
 
 const AboutPage = () => {
-  return <div></div>;
+  return <Card>AboutePage
+      <p>this is about us page </p>
+      <p>version 1.0.0</p>
+      <p><a href="/">Back to home</a></p>
+  </Card>;
 };
 
 export default AboutPage;
