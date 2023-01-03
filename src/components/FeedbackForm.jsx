8229b70f700b36ead4d7c1514b1abf5fb413e96f
anonymousdomain <dawityitagesu@gmail.com>
2023-01-03 19:00:06 +0300

    add review

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,42 @@
 import React, { useState } from "react";
 import Button from "./Button";
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 
-const FeedbackForm = () => {
-    const [text,setText]=useState()
+const FeedbackForm = ({ handelAdd }) => {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
   const handleChange = (e) => {
-    setText(e.target.value)
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("please write your review with more than 10 characters");
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ( text.trim().length >= 10) {
+      const newFeedBack = {
+        text,
+        rating,
+      };
+      handelAdd(newFeedBack);
+    }
+    setText("");
   };
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us </h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleChange}
@@ -18,9 +44,11 @@ const FeedbackForm = () => {
             placeholder="write a review"
             value={text}
           />
-          {/* <button type="submit">send</button> */}
-          <Button type='submit' >send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
