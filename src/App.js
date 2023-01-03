edbacaaa469f8f8c722368a6d1b2c915ed26d859
anonymousdomain <dawityitagesu@gmail.com>
2023-01-03 11:37:37 +0300

    revert back

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
-import FeedbackForm from "./components/FeedbackForm";
-import FeedbackList from "./components/FeedbackList";
-import FeedbackStats from "./components/FeedbackStats";
-import Header from "./components/Header";
-import { FeedbackProvider } from "./context/FeedbackContext";
-const App = () => {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <FeedbackProvider>
-      <Header />
-      <div className="container">
-        <FeedbackForm />
-        <FeedbackStats />
-        <FeedbackList />
-      </div>
-    </FeedbackProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
